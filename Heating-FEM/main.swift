345b42fc6df250ad85e983860301ca0160449d90
maikelSoFly <mikolaj.stepniewski1@gmail.com>
2018-01-20 19:06:50 +0100

    Initial commit

--- Heating-FEM/main.swift
+++ Heating-FEM/main.swift
@@ -10,6 +10,54 @@ import Foundation
 
 var globalData:GlobalData? = nil
 
+//MARK: - Main program.
+func run() {
+    if let dict = JsonParser.getDictionary(fromFile: "data", ofType: "json") {
+        if let gd = GlobalData(dict: dict) {
+            globalData = gd
+            gd.createGrid(materialDefinition: elementMaterialDefinition)
+            
+            /// Temperatures of every node in grid.
+            var temperatures:[Double]
+            let noTimeSteps = gd.tau/gd.d_tau
+            var heatMap = Array(repeating: Array(repeating: Array(repeating: Double(),
+                                count: gd.nH),
+                                count: gd.nB),
+                                count: Int(noTimeSteps)+1)
+            
+            for (step, tau) in stride(from: 0, to: gd.tau+gd.d_tau, by: gd.d_tau).enumerated() {
+                print("[time: \(tau) s]")
+                
+                for i in (0 ..< gd.nB).reversed() {
+                    for j in (0 ..< gd.nH) {
+                        let node = gd.grid.ND[j*gd.nH+i]
+                        heatMap[step][i][j] = node.temp
+                        
+                        print(String(format: "%.2f", node.temp), terminator:"  ")
+                    }
+                    print()
+                }
+                print("\n\n")
+                
+                gd.compute()
+                temperatures = Solver.gaussElimination(n: gd.nh, gk: gd.H_global, rk: gd.P_global)
+                
+                //MARK: - Set temperatures to nodes.
+                for (i, node) in gd.grid.ND.enumerated() {
+                    node.temp = temperatures[i]
+                }
+                
+            }
+            //TODO: Save heatMap to .csv
+            print(heatMap[5])
+        } else {
+            print("Typo in JSON data.")
+        }
+    } else {
+        print("JsonParser returned nil.")
+    }
+}
+
 
 /// Function which defines parameters for certain element, based
 /// on its location in grid array.
@@ -45,49 +93,8 @@ private func elementMaterialDefinition(i:Int, j:Int, nB:Int, nH:Int) -> Dictiona
 }
 
 
-//MARK: - Main program.
-if let dict = JsonParser.getDictionary(fromFile: "data", ofType: "json") {
-    if let gd = GlobalData(dict: dict) {
-        globalData = gd
-        gd.createGrid(materialDefinition: elementMaterialDefinition)
-        
-        /// Temperatures of every node in grid.
-        var temperatures:[Double]
-        let noTimeSteps = gd.tau/gd.d_tau
-        var heatMap = Array(repeating: Array(repeating: Array(repeating: Double(),
-                                                              count: gd.nH),
-                                             count: gd.nB),
-                            count: Int(noTimeSteps)+1)
-        
-        for (step, tau) in stride(from: 0, to: gd.tau+gd.d_tau, by: gd.d_tau).enumerated() {
-            print("[time: \(tau) s]")
-            
-            for i in (0 ..< gd.nB).reversed() {
-                for j in (0 ..< gd.nH) {
-                    let node = gd.grid.ND[j*gd.nH+i]
-                    heatMap[step][i][j] = node.temp
-                    
-                    print(String(format: "%.2f", node.temp), terminator:"  ")
-                }
-                print()
-            }
-            print("\n\n")
-            
-            gd.compute()
-            temperatures = Solver.gaussElimination(n: gd.nh, gk: gd.H_global, rk: gd.P_global)
-            
-            //MARK: - Set temperatures to nodes.
-            for (i, node) in gd.grid.ND.enumerated() {
-                node.temp = temperatures[i]
-            }
-            
-        }
-        //TODO: Save heatMap to .csv
-        print(heatMap[5])
-    } else {
-        print("Typo in JSON data.")
-    }
-} else {
-    print("JsonParser returned nil.")
-}
+run()
+
+
+
 
