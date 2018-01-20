75778529584cdd791048ab9ae93fbbd413508764
maikelSoFly <mikolaj.stepniewski1@gmail.com>
2018-01-20 17:04:58 +0100

    Initial Commit

---
+++ Heating-FEM/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Heating-FEM
+//
+//  Created by Mikołaj Stępniewski on 20/01/2018.
+//  Copyright © 2018 Mikołaj Stępniewski. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
